62ed7cb4997ec89913198a6555ecaa37c017f40d
Julia Ayres <j.bobrineva@gmail.com>
2020-12-18 10:25:49 -0500

    arrow functions

---
+++ app.js
@@ -0,0 +1,13 @@
+//ES5 Map Callback
+function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+}
+
+//ES2015 Arrow Functions Shorthand
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions:
+const squareAndFindEvens = numbers =>
+    numbers.map(num => num ** 2).filter(square => square % 2 === 0);
